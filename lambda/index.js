cf6f90e94d089dbae5cfe854002b00f8da90a4e3
Henry Jordan <henry.jordan@zefr.com>
2015-08-08 18:13:27 -0700

    updated example

--- lambda/index.js
+++ lambda/index.js
@@ -1,20 +1,19 @@
 var alexa = require('alexa-app');
 var request_lib = require('request');
-var striptags = require('striptags');
 
 // Make an app instance
 var app = new alexa.app();
 
 // Launch runs when a user queries the skill without an intent
 app.launch(function(request,response) {
-	response.say("I can poll an API and then format and read the response. Right now I am an example")
+	response.say("I can poll an API and then format and read the response")
 });
 
 // An intent
 app.intent('ExampleIntent',
   {
     "slots":{}
-    ,"utterances":[ "{try|test} me out", "run a test" ]
+    ,"utterances":[ "{try|test} me out", "run a test", "test" ]
   },
   function(request,response) {
 		request_lib.get(
@@ -22,8 +21,8 @@ app.intent('ExampleIntent',
 				url: 'http://example.com/alexa/',
 			},
 			function(err, httpResponse, body){
-				response.say(body);
-				response.card("Example", body);
+				response.say(decodeURI(body));
+				response.card("Example", decodeURI(body));
 				response.send();
 		 	}
 		);
@@ -40,5 +39,5 @@ app.sessionEnded(function(request,response) {
 // Connect to lambda
 exports.handler = app.lambda();
 
-console.log(app.schema())
-console.log(app.utterances())
+console.log("Schema: \n" + app.schema() + "\n")
+console.log("Utterances: \n" + app.utterances())
