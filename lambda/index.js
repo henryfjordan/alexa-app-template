15686004da328b02ad251adb77aee12ba4307436
Henry Jordan <henry.jordan@zefr.com>
2015-08-09 01:06:48 -0700

    tabs to spaces

--- lambda/index.js
+++ lambda/index.js
@@ -6,48 +6,48 @@ var app = new alexa.app();
 
 // Launch runs when a user queries the skill without an intent
 app.launch(function(request, response) {
-	httpRequest.get(
-		{
-			url: "http://urchin.henryfjordan.com/alexa",
-		},
-		function(err, httpResponse, body){
-			response.say(decodeURI(body));
-			response.card("Example", decodeURI(body));
-			response.send();
-		}
-	);
+    httpRequest.get(
+        {
+            url: "http://urchin.henryfjordan.com/alexa",
+        },
+        function(err, httpResponse, body){
+            response.say(decodeURI(body));
+            response.card("Example", decodeURI(body));
+            response.send();
+        }
+    );
 
-	return false;
+    return false;
 });
 
 // An intent
 app.intent('ExampleIntent',
   {
     "slots":{"NAME":"LITERAL"},
-		"utterances":[ "my {name is|name's} {name|NAME}"]
+        "utterances":[ "my {name is|name's} {name|NAME}"]
   },
   function(request, response) {
 
-		var name = request.slot('NAME');
+        var name = request.slot('NAME');
 
-		httpRequest.get(
-			{
-				url: "http://urchin.henryfjordan.com/alexa/" + encodeURIComponent(name),
-			},
-			function(err, httpResponse, body){
-				response.say(decodeURI(body));
-				response.card("Example", decodeURI(body));
-				response.send();
-		 	}
-		);
+        httpRequest.get(
+            {
+                url: "http://urchin.henryfjordan.com/alexa/" + encodeURIComponent(name),
+            },
+            function(err, httpResponse, body){
+                response.say(decodeURI(body));
+                response.card("Example", decodeURI(body));
+                response.send();
+             }
+        );
 
-		return false;
+        return false;
   }
 );
 
 // sessionEnded runs when a sessions ends
 app.sessionEnded(function(request,response) {
-		// Here is where you might clean up user information or logout of a service
+        // Here is where you might clean up user information or logout of a service
 });
 
 // Connect to lambda
